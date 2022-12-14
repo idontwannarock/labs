7865f883d4e56c987771d9d8f425a266d6909dde
Howard Wang <idontwannarock@gmail.com>
2022-12-14 16:58:45 +0800

    feat: init lab-spring-boot-gradle-docker

---
+++ lab-spring-boot-gradle-docker/src/main/java/com/example/lab/spring/boot/gradle/docker/LabSpringBootGradleDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab.spring.boot.gradle.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabSpringBootGradleDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabSpringBootGradleDockerApplication.class, args);
+    }
+
+}
