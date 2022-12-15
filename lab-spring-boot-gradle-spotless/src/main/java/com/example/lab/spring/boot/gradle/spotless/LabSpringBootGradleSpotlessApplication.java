bbb4faf01c39fa3116b25850f7c4dc36c76463c4
Howard Wang <idontwannarock@gmail.com>
2022-12-15 13:54:04 +0800

    feat: init lab-spring-boot-gradle-spotless

---
+++ lab-spring-boot-gradle-spotless/src/main/java/com/example/lab/spring/boot/gradle/spotless/LabSpringBootGradleSpotlessApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab.spring.boot.gradle.spotless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabSpringBootGradleSpotlessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabSpringBootGradleSpotlessApplication.class, args);
+    }
+
+}
---
+++ lab-spring-boot-gradle-spotless/src/test/java/com/example/lab/spring/boot/gradle/spotless/LabSpringBootGradleSpotlessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab.spring.boot.gradle.spotless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabSpringBootGradleSpotlessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
